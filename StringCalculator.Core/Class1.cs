f7b74436428f1bc8456b92cd96d5b6c366e12be6
Anith Thankachen <anithtt@gmail.com>
2023-09-29 23:53:37 +0100

    Initial changes  - Creation of web application

---
+++ StringCalculator.Core/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace StringCalculator.Core
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ StringCalculator.Core/Interfaces/IMathExpressionEvaluator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator.Core.Interfaces
+{
+    public interface IMathExpressionEvaluator
+    {
+        double Evaluate(string expression);
+    }
+    public class MathExpressionEvaluator : IMathExpressionEvaluator
+    {
+        public double Evaluate(string expression)
+        {
+            // Implement the expression evaluation logic here
+            DataTable table = new DataTable();
+            table.Columns.Add("expression", typeof(string), expression);
+            DataRow row = table.NewRow();
+            table.Rows.Add(row);
+            return double.Parse((string)row["expression"]);
+        }
+    }
+}
